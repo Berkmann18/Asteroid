4183b78f3b8b619149c64db52b43b8e7fc71fed1
Maximilian Berkmann <maxieberkmann@gmail.com>
2016-03-01 17:42:26 +0000

    Update Spaceship.py

--- Spaceship.py
+++ Spaceship.py
@@ -1,9 +1,11 @@
+import Vector
 try:
     import simplegui
 except ImportError:
     import SimpleGUICS2Pygame.simpleguics2pygame as simplegui
 
 img = simplegui.load_image("http://www.cs.rhul.ac.uk/courses/CS1830/asteroids/double_ship.png")
+missileImg = simplegui.load_image("http://www.cs.rhul.ac.uk/courses/CS1830/asteroids/shot3.png")
 imgSize = (180, 90)
 imgCentre = (90, 45)
 frameWidth = imgSize[0]/2 #2 columns
@@ -14,8 +16,8 @@ canvasSize = (600, 400)
 
 class Spaceship:
     def __init__(self, p, v):
-        self.pos = p
-        self.vel = v
+        self.pos = Vector(p)
+        self.vel = Vector(v)
         self.lives = 3
         self.moveUp = False
         self.moveDown = False
@@ -33,17 +35,17 @@ class Spaceship:
 
         x = frameWidth*j+frameWidth/2
         y = frameHeight/2
-        canvas.draw_image(img, (x, y), (frameWidth, frameHeight), (canvasSize[0]/2, canvasSize[1]/2), (frameWidth, frameHeight))
+        canvas.draw_image(img, (x, y), (frameWidth, frameHeight), self.pos.getP(), (frameWidth, frameHeight))
 
 
     def hit(self, asteroid):
         pass
 
     def offset(self, c):
-        if c == 'l': return self.pos.x#-width of the image
-        elif c == 'r': return self.pos.x#+width of the image
-        elif c == 'u': return self.pos.y#-height of the image
-        else: return self.pos.y#+height of the image
+        if c == 'l': return self.pos.x-frameWidth/2
+        elif c == 'r': return self.pos.x+frameWidth/2
+        elif c == 'u': return self.pos.y-frameHeight/2
+        else: return self.pos.y+frameHeight/2
 
 
 class Missile:
@@ -55,17 +57,13 @@ class Missile:
         self.pos.add(self.vel)
 
     def draw(self, canvas):
-        pass
+        canvas.draw_image(missileImg, (10, 10), (20, 20), self.pos.getP(), (20, 20))
 
     def hit(self, asteroid):
-        pass
+        return not ((asteroid.pos.x>self.pos.x+self.pos.w) or (asteroid.pos.x+asteroid.pos.w<self.pos.x) or (asteroid.pos.y>self.pos.y+self.pos.h) or (asteroid.pos.y+asteroid.pos.h<self.pos.y))
 
     def offset(self, c):
-        if c == 'l':
-            return self.pos.x  # -width of the image
-        elif c == 'r':
-            return self.pos.x  # +width of the image
-        elif c == 'u':
-            return self.pos.y  # -height of the image
-        else:
-            return self.pos.y  # +height of the image
+        if c == 'l': return self.pos.x-10
+        elif c == 'r': return self.pos.x+10
+        elif c == 'u': return self.pos.y-10
+        else: return self.pos.y+10
