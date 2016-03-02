4aea769dfc889438ac64df1cc0b77a2c12c6fae6
Maximilian Berkmann <maxieberkmann@gmail.com>
2016-03-02 18:40:12 +0000

    Update Rock.py
    Added the SimpleGUI support and an example image

--- Rock.py
+++ Rock.py
@@ -1,18 +1,21 @@
 import Vector
+try:
+    import simplegui
+except ImportError:
+    import SimpleGUICS2Pygame.simpleguics2pygame as simplegui
+
+img = simplegui.load_image("http://www.cs.rhul.ac.uk/courses/CS1830/asteroids/asteroid_blue.png")
 
 class Rock:
     def __init__(self, p, v):
         self.pos = Vector(p)
         self.vel = Vector(v)
-
+        self.size = 90
     def update(self):
         self.pos.add(self.vel)
 
     def draw(self, canvas):
-        pass
-
-    def hit(self, asteroid):
-        pass
+        canvas.draw_image(img, (self.size/2, self.size/2), (self.size, self.size), self.pos.getP(), (self.size, self.size))
 
     def offset(self, c):
         if c=='l': return self.pos.x#-width of the image
