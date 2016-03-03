24be954641c8ebdc841d7900185f8bf9b546c2d3
Maximilian Berkmann <maxieberkmann@gmail.com>
2016-03-03 14:37:55 +0000

    Update Rock.py

--- Rock.py
+++ Rock.py
@@ -8,10 +8,10 @@ img = simplegui.load_image("http://www.cs.rhul.ac.uk/courses/CS1830/asteroids/as
 
 class Rock:
     def __init__(self, p, v):
-        self.pos = Vector(p)
-        self.vel = Vector(v)
+        self.pos = Vector.Vector(p)
+        self.vel = Vector.Vector(v)
         self.size = 90
-        
+
     def update(self):
         self.pos.add(self.vel)
 
@@ -19,7 +19,7 @@ class Rock:
         canvas.draw_image(img, (self.size/2, self.size/2), (self.size, self.size), self.pos.getP(), (self.size, self.size))
 
     def offset(self, c):
-        if c=='l': return self.pos.x-self.size/2
-        elif c=='r': return self.pos.x+self.size/2
-        elif c=='u': return self.pos.y-self.size/2
+        if c == 'l': return self.pos.x-self.size/2
+        elif c == 'r': return self.pos.x+self.size/2
+        elif c == 'u': return self.pos.y-self.size/2
         else: return self.pos.y+-self.size/2
