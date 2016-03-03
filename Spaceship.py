13b4cb117ceb51de8b3beacc1b903340f20737ad
Maximilian Berkmann <maxieberkmann@gmail.com>
2016-03-03 22:52:02 +0000

    Update Spaceship.py
    Pylint evaluation: 9.86/10
    l.18/c.0: too many instance attributes (12/7)

--- Spaceship.py
+++ Spaceship.py
@@ -14,8 +14,6 @@ IMG = simplegui.load_image("spaceship.png")
 MISSILE_IMG = simplegui.load_image("http://www.cs.rhul.ac.uk/courses/CS1830/asteroids/shot3.png")
 IMG_SIZE = (180, 90)
 IMG_CENTRE = (90, 45)
-FRAME_X = IMG_SIZE[0]/4
-FRAME_Y = IMG_SIZE[1]/2
 
 class Spaceship:
     """
@@ -32,6 +30,8 @@ class Spaceship:
         self.missiles = []
         self.width = IMG_SIZE[0]/2 #2 columns
         self.height = IMG_SIZE[1]
+        self.frame_x = IMG_SIZE[0]/4
+        self.frame_y = IMG_SIZE[1]/2
 
     def update(self):
         """
@@ -43,15 +43,14 @@ class Spaceship:
         """
         Draw the spaceship and its missiles
         """
-        global FRAME_X, FRAME_Y
         # Decide which image is going to be shown depending on the spaceship's state
         j = 0 if self.rot_ccw or self.rot_cw or self.forward else 1
         self.width = IMG_SIZE[0]/2
         self.height = IMG_SIZE[1]
 
-        FRAME_X = self.width*j+self.width/2
-        FRAME_Y = self.height/2
-        canvas.draw_image(IMG, (FRAME_X, FRAME_Y), (self.width, self.height),\
+        self.frame_x = self.width*j+self.width/2
+        self.frame_y = self.height/2
+        canvas.draw_image(IMG, (self.frame_x, self.frame_y), (self.width, self.height),\
         self.pos.get_pt(), (self.width, self.height))
         for msl in self.missiles:
             msl.draw(canvas)
