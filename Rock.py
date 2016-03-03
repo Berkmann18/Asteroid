8e7c68dbcd472247c49653cdc0049c584ff08907
Maximilian Berkmann <maxieberkmann@gmail.com>
2016-03-03 21:39:10 +0000

    Update Rock.py
    Pylint evaluation: 10/10

--- Rock.py
+++ Rock.py
@@ -1,25 +1,48 @@
-import Vector
+"""
+Rock.py
+The module that implements asteroids
+"""
+
 try:
     import simplegui
 except ImportError:
     import SimpleGUICS2Pygame.simpleguics2pygame as simplegui
+import Vector
 
-img = simplegui.load_image("http://www.cs.rhul.ac.uk/courses/CS1830/asteroids/asteroid_blue.png")
+IMG = simplegui.load_image("\
+http://www.cs.rhul.ac.uk/courses/CS1830/asteroids/asteroid_blue.png")
 
 class Rock:
+    """
+    Rock class
+    """
     def __init__(self, p, v):
         self.pos = Vector.Vector(p)
         self.vel = Vector.Vector(v)
         self.size = 90
 
     def update(self):
+        """
+        Updates the position of the rock
+        """
         self.pos.add(self.vel)
 
     def draw(self, canvas):
-        canvas.draw_image(img, (self.size/2, self.size/2), (self.size, self.size), self.pos.getP(), (self.size, self.size))
+        """
+        Draw the rock
+        """
+        canvas.draw_image(IMG, (self.size/2, self.size/2), (self.size, self.size),\
+        self.pos.get_pt(), (self.size, self.size))
 
-    def offset(self, c):
-        if c == 'l': return self.pos.x-self.size/2
-        elif c == 'r': return self.pos.x+self.size/2
-        elif c == 'u': return self.pos.y-self.size/2
-        else: return self.pos.y+-self.size/2
+    def offset(self, char):
+        """
+        Offsets of the rock
+        """
+        if char == 'l':
+            return self.pos.x_coord-self.size/2
+        elif char == 'r':
+            return self.pos.x_coord+self.size/2
+        elif char == 'u':
+            return self.pos.y_coord-self.size/2
+        else:
+            return self.pos.y_coord+-self.size/2
