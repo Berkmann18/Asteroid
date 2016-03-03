fc61e52417a9e910643884ce06f5801cada47626
Maximilian Berkmann <maxieberkmann@gmail.com>
2016-03-03 22:15:58 +0000

    Update Asteroid.py
    Pylint evaluation: 9.77/10
    
    l.59/c.0: Too many args (6/5)
    l.73/c.8: Unused variable 'i'

--- Asteroid.py
+++ Asteroid.py
@@ -1,30 +1,44 @@
-#Main class
+"""
+Asteroid.py
+Main class of the Asteroid GAME
+"""
 
-import Rock
-import Spaceship
 import random
-
 try:
     import simplegui
 except ImportError:
-    import SimpleGUICS2Pygame.simpleguics2pygame as simplegui
+    import SimpleGUICS2PyGAME.simpleguics2pyGAME as simplegui
 
-canvasSize = (600, 400)
+import Rock
+import Spaceship
+
+CANVAS_SIZE = (600, 400)
 
 class Interaction:
+    """
+    Interaction between the spaceship and the rocks
+    """
     def __init__(self, spaceship, rocks):
         self.spaceship = spaceship
         self.rocks = rocks
 
     def draw(self, canvas):
+        """
+        Draw everything needed for the interaction
+        """
         self.spaceship.draw(canvas)
-        for r in self.rocks: r.draw(canvas)
+        for rocks in self.rocks:
+            rocks.draw(canvas)
 
     def update(self):
-        for r in self.rocks:
-            if self.spaceship.hit(r): self.spaceship.lives -= 1 #take a life off the spaceship
+        """
+        Update everything needed for the interaction
+        """
+        for rocks in self.rocks:
+            if self.spaceship.hit(rocks):
+                self.spaceship.lives -= 1 #take a life off the spaceship
         if self.spaceship.lives <= 0: #start over
-            game.welcomeScreen["enabled"] = True
+            GAME.welcome_screen["enabled"] = True
             self.rocks = [] #remove all rocks
 
         if self.spaceship.rotCW: #clockwise rotation of the spaceship
@@ -35,67 +49,108 @@ class Interaction:
             self.spaceship.update()
         if self.spaceship.gunLoaded: #shoot a missile
             self.spaceship.shoot()
-            if self.spaceship.missiles[len(self.spaceship.missiles)-1].pos.x > canvasSize[0] or self.spaceship.missiles[len(self.spaceship.missiles)-1].pos.x < 0 or self.spaceship.missiles[len(self.spaceship.missiles)-1].pos.y > canvasSize[1] or self.spaceship.missiles[len(self.spaceship.missiles)-1].pos.y < 0:
+            if self.spaceship.missiles[len(self.spaceship.missiles)-1].pos.x_coord > CANVAS_SIZE[0]\
+            or self.spaceship.missiles[len(self.spaceship.missiles)-1].pos.x_coord < 0\
+            or self.spaceship.missiles[len(self.spaceship.missiles)-1].pos.y_coord > CANVAS_SIZE[1]\
+            or self.spaceship.missiles[len(self.spaceship.missiles)-1].pos.y_coord < 0:
                 self.spaceship.missiles.pop() #remove the missile that went off-screen
 
 
-def rect(canvas, a, b, border, borderClr, bgClr): #draw a rectangle
-    canvas.draw_polygon([a, (b[0], a[1]), b, (a[0], b[1])], border, borderClr)
-    for i in range(a[0]+1, b[0]-1):
-        canvas.draw_line((i, a[1]), (i, b[1]), 2, bgClr)
+def rect(canvas, pt_a, pt_b, pt_border, pt_border_clr, pt_bg_clr):
+    """
+    Drpt_aw pt_a rectpt_angle
+    """
+    canvas.draw_polygon([pt_a, (pt_b[0], pt_a[1]), pt_b, (pt_a[0], pt_b[1])],\
+	pt_border, pt_border_clr)
+    for i in range(pt_a[0]+1, pt_b[0]-1):
+        canvas.draw_line((i, pt_a[1]), (i, pt_b[1]), 2, pt_bg_clr)
 
-def RandRocks(): #generate rocks at random positions
-    rr = []
-    for i in range(random.randint(12)):
-        x = random.randrange(0, canvasSize[0])
-        y = random.randrange(0, canvasSize[1])
-        while Inter.spaceship.zone()[1] <= x <= Inter.spaceship.zone()[3]: x = random.randrange(0, canvasSize[0])
-        while Inter.spaceship.zone()[0] <= y <= Inter.spaceship.zone()[2]: y = random.randrange(0, canvasSize[1])
-        rr.append(Rock.Rock((x, y), (0, 0)))
-    return rr
+def randrocks():
+    """
+    Generate rocks at random positions
+    """
+    arr = []
+    for i in range(random.randint(0, 12)):
+        x_coord = random.randrange(0, CANVAS_SIZE[0])
+        y_coord = random.randrange(0, CANVAS_SIZE[1])
+        while INTER.spaceship.zone()[1] <= x_coord <= INTER.spaceship.zone()[3]:
+            x_coord = random.randrange(0, CANVAS_SIZE[0])
+        while INTER.spaceship.zone()[0] <= y_coord <= INTER.spaceship.zone()[2]:
+            y_coord = random.randrange(0, CANVAS_SIZE[1])
+        arr.append(Rock.Rock((x_coord, y_coord), (0, 0)))
+    return arr
 
-Inter = Interaction(Spaceship.Spaceship((canvasSize[0]/2, canvasSize[1]/2), (0, 0)), RandRocks())
+INTER = Interaction(Spaceship.Spaceship((CANVAS_SIZE[0]/2, CANVAS_SIZE[1]/2), (0, 0)), randrocks())
 
 class Game:
+    """
+    Game class containing the UI and key management system
+    """
     def __init__(self):
-        self.welcomeScreen["enabled"] = True
-        self.welcomeScreen = dict(enabled=True, x=canvasSize[0]/4, y=canvasSize[1]/4, width=canvasSize[0]/2, height=canvasSize[1]/2)
+        self.welcome_screen = dict(enabled=True, x=CANVAS_SIZE[0]/4,\
+        y=CANVAS_SIZE[1]/4, width=CANVAS_SIZE[0]/2,\
+        height=CANVAS_SIZE[1]/2)
         self.score = 0
 
     def draw(self, canvas):
-        if self.welcomeScreen["enabled"]: #display the welcome screen
-            pass
-            self.welcomeScreen["enabled"] = False
+        """
+        UI
+        """
+        if self.welcome_screen["enabled"]: #display the welcome screen
+            self.welcome_screen["enabled"] = False
         else:
-            Inter.draw(canvas)
-            canvas.draw_text("Score: "+str(self.score), (canvasSize[0]-450, 50), 25, "White", "sans-serif")
-            canvas.draw_text("Lives: "+str(Inter.spaceship.lives), (50, 50), 25, "White", "sans-serif")
+            INTER.draw(canvas)
+            canvas.draw_text("Score: "+str(self.score),\
+			(CANVAS_SIZE[0]-450, 50), 25, "White", "sans-serif")
+            canvas.draw_text("Lives: "+str(INTER.spaceship.lives),\
+			(50, 50), 25, "White", "sans-serif")
 
-    def mouse(self, p): #mouse handler
-        if self.welcomeScreen["enabled"] and self.welcomeScreen["x"] <= p[0] <= self.welcomeScreen["x"]+self.welcomeScreen["width"] and self.welcomeScreen["y"] <= p[1] <= self.welcomeScreen["y"]+self.welcomeScreen["height"]:
-            Inter.spaceship.lives = 3
+    def mouse(self, pos):
+        """
+        Mouse handler
+        """
+        if self.welcome_screen["enabled"] and\
+		self.welcome_screen["x"] <= pos[0] <= self.welcome_screen["x"]+self.welcome_screen["width"]\
+		and self.welcome_screen["y"] <= pos[1] <= self.welcome_screen["y"]+self.welcome_screen["height"]:
+            INTER.spaceship.lives = 3
             self.score = 0
 
-    def keyDown(self, key): #key down handler
-        global Inter
-        if key == simplegui.KEY_MAP['left']: Inter.spaceship.rotCCW = True #spaceship rotates at a constant angular velocity in a counter clockwise direction
-        if key == simplegui.KEY_MAP['right']: Inter.spaceship.rotCW = True #spaceship rotates at a constant angular velocity in a clockwise direction
-        if key == simplegui.KEY_MAP['up']: Inter.spaceship.forward = True #spaceship accelerates in its forward direction
-        if key == simplegui.KEY_MAP['space']: Inter.spaceship.gunLoaded = True #missile is spawned at the tip of the spaceship's gun
+def keydown(key):
+    """
+    Key down handler
+    """
+    if key == simplegui.KEY_MAP['left']:
+	    # Spaceship rotates at a constant angular velocity in a counter clockwise direction
+        INTER.spaceship.rotCCW = True
+    if key == simplegui.KEY_MAP['right']:
+	    # Spaceship rotates at a constant angular velocity in a clockwise direction
+        INTER.spaceship.rotCW = True
+    if key == simplegui.KEY_MAP['up']:
+	    # Spaceship accelerates in its forward direction
+        INTER.spaceship.forward = True
+    if key == simplegui.KEY_MAP['space']:
+	    # Missile is spawned at the tip of the spaceship's gun
+        INTER.spaceship.gunLoaded = True
 
-    def keyUp(self, key): #key up handler
-        global Inter
-        if key == simplegui.KEY_MAP['left']: Inter.spaceship.rotCCW = False
-        if key == simplegui.KEY_MAP['right']: Inter.spaceship.rotCW = False
-        if key == simplegui.KEY_MAP['up']: Inter.spaceship.forward = False
-        if key == simplegui.KEY_MAP['space']: Inter.spaceship.gunLoaded = False
+def keyup(key):
+    """
+    key up handler
+    """
+    if key == simplegui.KEY_MAP['left']:
+        INTER.spaceship.rotCCW = False
+    if key == simplegui.KEY_MAP['right']:
+        INTER.spaceship.rotCW = False
+    if key == simplegui.KEY_MAP['up']:
+        INTER.spaceship.forward = False
+    if key == simplegui.KEY_MAP['space']:
+        INTER.spaceship.gunLoaded = False
 
-game = Game()
+GAME = Game()
 
 #Frame property specification
-frame = simplegui.create_frame("Home", canvasSize[0], canvasSize[1])
-frame.set_draw_handler(game.draw)
-frame.set_mouseclick_handler(game.mouse)
-frame.set_keydown_handler(game.keyDown)
-frame.set_keyup_handler(game.keyUp)
-frame.start()
+FRAME = simplegui.create_frame("Asteroid", CANVAS_SIZE[0], CANVAS_SIZE[1])
+FRAME.set_draw_handler(GAME.draw)
+FRAME.set_mouseclick_handler(GAME.mouse)
+FRAME.set_keydown_handler(keydown)
+FRAME.set_keyup_handler(keyup)
+FRAME.start()
