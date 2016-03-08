390a3c2451741f54f2deb3fa2f65acb3dc865aaf
Maximilian Berkmann <maxieberkmann@gmail.com>
2016-03-08 23:10:19 +0000

    Update Rock.py

--- Rock.py
+++ Rock.py
@@ -7,9 +7,10 @@ try:
     import simplegui
 except ImportError:
     import SimpleGUICS2Pygame.simpleguics2pygame as simplegui
+import Util
 import Vector
 
-IMG = simplegui.load_image("http://www.cs.rhul.ac.uk/courses/CS1830/asteroids/asteroid_blue.png")
+IMG = simplegui.load_image("http://www.cs.rhul.ac.uk/courses/CS1830/asteroids/asteroid_brown.png")
 
 class Rock:
     """
@@ -18,7 +19,7 @@ class Rock:
     def __init__(self, p, v):
         self.pos = Vector.Vector(p)
         self.vel = Vector.Vector(v)
-        self.size = 90
+        self.radius = 45
 
     def update(self):
         """
@@ -30,18 +31,63 @@ class Rock:
         """
         Draw the rock
         """
-        canvas.draw_image(IMG, (self.size/2, self.size/2), (self.size, self.size), self.pos.get_pt(),\
-        (self.size, self.size))
+        canvas.draw_image(IMG, (self.radius, self.radius), (self.radius*2, self.radius*2),\
+                          self.pos.get_pt(), (self.radius*2, self.radius*2))
 
     def offset(self, char):
         """
         Offsets of the rock
         """
         if char == 'l':
-            return self.pos.x-self.size/2
+            return self.pos.x-self.radius
         elif char == 'r':
-            return self.pos.x+self.size/2
+            return self.pos.x+self.radius
         elif char == 'u':
-            return self.pos.y-self.size/2
+            return self.pos.y-self.radius
         else:
-            return self.pos.y+-self.size/2
+            return self.pos.y+self.radius
+
+    def get_ball(self):
+        """
+        Get the collision circle
+        """
+        return Util.Ball(self.pos, self.vel, .8*self.radius, 1, "rgba(255, 255, 255, 0)", "red")
+
+    def draw_circ(self, canvas):
+        """
+        Draw the collision circle
+        """
+        self.get_ball().draw(canvas)
+
+    def collide(self, rock):
+        """
+        Collide against an other rock
+        """
+        vct_u = self.vel.copy()
+        vct_v = rock.vel.copy()
+        norm = (self.pos.copy().sub(rock.pos)).get_normal()
+        vct_z = norm.copy().mult(self.pos.copy().sub(rock.pos).dot(norm))
+
+        vct_u.add(vct_z)
+        vct_v.sub(vct_z)
+
+        self.vel = vct_u
+        rock.vel = vct_v
+
+    def hit(self, rck):
+        """
+        Rock/rock collision detection
+        """
+        return self.get_ball().hit(rck.get_ball())
+
+    def get_child(self):
+        """
+        Make 4 childrens that goes different ways
+        """
+        childs = [
+            Rock(self.pos.get_pt(), (2, 2)), Rock(self.pos.get_pt(), (2, -2)),
+            Rock(self.pos.get_pt(), (-2, 2)), Rock(self.pos.get_pt(), (-2, -2))
+        ]
+        for child in childs:
+            child.radius = self.radius/2
+            child.update()
